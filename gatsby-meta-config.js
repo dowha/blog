eaa1c5e2ee9cd8735991cd34d530082c96aeab3c
Dowha Kim <dowha.kim@outlook.com>
2019-07-06 22:51:41 +0700

    Change styles and metadata

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  title: `Bee starter`,
-  description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  title: `파도는 멈추지 않고`,
+  description: `Dowha Kim's blog containing various articles. `,
+  author: `Dowha`,
+  introduction: `A Communication Specialist working in LGBTI rights field.`,
+  siteUrl: `https://dowha.netlify.com/`, // Your blog site url
   social: {
-    twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    twitter: `Dowha_Kim`, // Your Twitter account
+    github: `dowha`, // Your GitHub account
     medium: ``, // Your Medium account
-    facebook: ``, // Your Facebook account
+    facebook: `kimdowha`, // Your Facebook account
   },
-  icon: `content/assets/felog.png`, // Add your favicon
+  icon: `content/assets/dowha-blog.png`, // Add your favicon
   keywords: [`blog`],
   comment: {
-    disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    disqusShortName: 'dowha-kim', // Your disqus-short-name. check disqus.com.
+    utterances: '', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: 'dowha',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -4,8 +4,8 @@ import './index.scss'
 
 export const Footer = () => (
   <footer className="footer">
-    ©<a href="https://github.com/JaeYeopHan">Jbee</a>, Built with{' '}
-    <a href="https://github.com/JaeYeopHan/gatsby-starter-bee">
+    ©<a href="https://dowha.kim">Dowha</a>, Built with{' '}
+    <a href="https://github.com/JaeYeopHan/gatsby-starter-bee" target="_blank">
       Gatsby-starter-bee
     </a>
   </footer>
