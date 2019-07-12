29b249414bd0f2e84b7efde18d9aea355f2e9ba3
Dowha Kim <dowha.kim@outlook.com>
2019-07-13 03:28:42 +0700

    Edit share to twitter

--- src/utils/share.js
+++ src/utils/share.js
@@ -11,7 +11,7 @@ export const shareToTwitter = (href, text) => {
   window.open(
     `https://twitter.com/share?url=${encodeURI(
       encodeURI(href)
-    )}&text=${text} @dowha_kim`,
+    )}&text=${text}&via=Dowha_Kim`,
     'sharer',
     'toolbar=0,status=0,width=626,height=436'
   )
