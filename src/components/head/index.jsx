af5c9f28994fdfd9ede1c546232d196c0fc4bcf5
Dowha Kim <dowha.kim@outlook.com>
2019-07-14 01:05:02 +0700

    Update

--- src/components/head/index.jsx
+++ src/components/head/index.jsx
@@ -60,13 +60,7 @@ export function Head({ description, lang, meta, keywords, title }) {
                   : []
               )
               .concat(meta)}
-          > <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-          <script>
-            (adsbygoogle = window.adsbygoogle || []).push({
-              google_ad_client: "ca-pub-9493475755258000",
-              enable_page_level_ads: true
-            });
-          </script> </Helmet>
+          />
         )
       }}
     />
