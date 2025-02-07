bb4f42f6db6d1067ca1d74d050154e6b9e403993
Dowha Kim <mail@dowha.kim>
2025-02-07 12:34:21 +0900

    Update writing.tsx

--- pages/writing.tsx
+++ pages/writing.tsx
@@ -26,27 +26,9 @@ export default function WritingPage({ posts }: Props) {
               href={post.external_url}
               target="_blank"
               rel="noopener noreferrer"
-              className="inline-flex items-center space-x-1"
+              className="inline-flex items-center"
             >
-              <span className="inline-flex items-end">
-                {post.title}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="ml-1 h-4 w-4 self-end"
-                >
-                  <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
-                  <polyline points="15 3 21 3 21 9" />
-                  <line x1="10" y1="14" x2="21" y2="3" />
-                </svg>
-              </span>
+              {post.title}
             </a>
           ) : (
             <Link href={`/post/${post.slug}`} className="flex items-center">
